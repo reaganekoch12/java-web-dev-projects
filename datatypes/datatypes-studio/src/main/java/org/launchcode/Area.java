3ac11955008a899c7501b45ac8bf5fc1fcd6bcd5
Reagan Koch <reaganekoch@gmail.com>
2023-08-09 19:22:00 -0500

    Creating Area of Circle

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -2,9 +2,10 @@ package org.launchcode;
 import java.util.Scanner;
 public class Area {
         public static void main(String[] args){
+            double radius;
             Scanner input = new Scanner(System.in);
             System.out.println("Enter a radius");
-            double radius = input.nextDouble();
+             radius = input.nextDouble();
         }
         public static double getArea(double radius){
             return 3.14 * radius * radius;
